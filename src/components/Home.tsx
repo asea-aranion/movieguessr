47bbedfd45aff7b9b9b88cebce13e889eba1fa36
Leia Spagnola <leiaspagnola@icloud.com>
2025-11-15 17:59:47 -0500

    style link

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <Link to="/MovieGuessr">Start playing!</Link>
+            <Link to="/MovieGuessr">Play MovieGuessr!</Link>
         </div>
     );
 }
