84474a2e61dff64f8661438b91feb2156da227ee
Leia Spagnola <leiaspagnola@icloud.com>
2025-11-16 09:38:32 -0500

    style leaderboard

--- src/components/LeaderboardsPage.tsx
+++ src/components/LeaderboardsPage.tsx
@@ -5,12 +5,13 @@ import { genres, getLeaderboard, getPlaceString } from "../utils";
 import DropDown from "./DropDown";
 
 const LeaderboardsPage = () => {
-    const [dateRange, setDateRange] = useState<LeaderboardDateRange>("all");
-	const [showDropdown, setShowDropdown] = useState(false);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [dateRange, _] = useState<LeaderboardDateRange>("all");
+    const [showDropdown, setShowDropdown] = useState(false);
     const [genre, setGenre] = useState(0);
     const [leaderboard, setLeaderboard] = useState<LeaderboardScore[]>([]);
 
-	const dismissHandler = (
+    const dismissHandler = (
         event: React.FocusEvent<HTMLButtonElement>
     ): void => {
         if (event.currentTarget === event.target) {
@@ -20,33 +21,41 @@ const LeaderboardsPage = () => {
 
     useEffect(() => {
         getLeaderboard(genre, dateRange).then((data) => setLeaderboard(data));
-		console.log(dateRange)
+        console.log(dateRange);
     }, [genre, dateRange]);
 
     return (
         <div className="leaderboards-cont">
-			<h1>Leaderboards</h1>
-			<button
-                    className={showDropdown ? "menu active leaderboard-dropdown" : "menu leaderboard-dropdown"}
-                    onClick={() => setShowDropdown(value => !value)}
-                    onBlur={(e: React.FocusEvent<HTMLButtonElement>): void =>
-                        dismissHandler(e)
-                    }
-                >
-                    <div>{genres.find(element => element.id === genre)?.name || ""}</div>
-                    {showDropdown && (
-                        <DropDown
-                            options={genres}
-                            setGenreId={setGenre}
-                            setGenreName={() => {}}
-                        />
-                    )}
-                </button>
+            <h1>Leaderboards</h1>
+            <button
+                className={
+                    showDropdown
+                        ? "menu active leaderboard-dropdown"
+                        : "menu leaderboard-dropdown"
+                }
+                onClick={() => setShowDropdown((value) => !value)}
+                onBlur={(e: React.FocusEvent<HTMLButtonElement>): void =>
+                    dismissHandler(e)
+                }
+            >
+                <div>
+                    {genres.find((element) => element.id === genre)?.name || ""}
+                </div>
+                {showDropdown && (
+                    <DropDown
+                        options={genres}
+                        setGenreId={setGenre}
+                        setGenreName={() => {}}
+                    />
+                )}
+            </button>
             {leaderboard.map((score) => (
                 <div className="leaderboard-card">
-                    <p className="leaderboard-position">{getPlaceString(score.position)}</p>
-                    <p>{score.username}</p>
-                    <p>{score.score}</p>
+                    <p className="card-text leaderboard-position">
+                        {getPlaceString(score.position)}
+                    </p>
+                    <p className="card-text">{score.username}</p>
+                    <p className="card-text">{score.score}</p>
                 </div>
             ))}
         </div>
